db0b5d397a9d0e1cb9eacd3b469d557fc1337c80
khy-hh <khyhuynh@gmail.com>
2023-06-16 16:55:11 +0000

    making requests with Django

---
+++ djreq/djreq/views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# python3 -m pip install requests
+import requests
+
+# python3 -m pip install Django
+from django.http import JsonResponse    # replaces "import json"
+
+# API to lookup - Django will proxy the request for us
+API = "http://api.open-notify.org/astros.json"
+
+def astro(request):    
+    res = requests.get(API)
+    return JsonResponse(res.json())  # abstraction to return json
+
